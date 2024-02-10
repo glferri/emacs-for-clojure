23b741dc6b9f1da03795072ee3760f3ccade9dc5
gferri <gferri@onit.it>
2024-02-10 02:31:56 +0100

    removed fixed path for spell checker

--- customizations/spell-checkers.el
+++ customizations/spell-checkers.el
@@ -1,8 +1,7 @@
 ;; following https://www.tenderisthebyte.com/blog/2019/06/09/spell-checking-emacs/#:~:text=The%20basic%20way%20to%20do,back%20to%20the%20default%20dictionary.
 
-;;; still not working, we could try installing hunspell
-
-(add-to-list 'exec-path "C:/Users/gferri/scoop/apps/msys2/current/usr/bin/")
+;better put msys bin dir in path
+;(add-to-list 'exec-path "C:/Users/gferri/scoop/apps/msys2/current/usr/bin/")
 (setq ispell-program-name "aspell")
 ;; enable flyspell
 (dolist (hook '(text-mode-hook))
