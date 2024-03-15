306b521722847983628f2288bfa1e0392d5771c5
Gianluca Ferri <glferri@gmail.com>
2024-03-15 06:46:06 +0100

    added not working autocomplete confing for org-roam

--- customizations/setup-org-roam.el
+++ customizations/setup-org-roam.el
@@ -3,13 +3,17 @@
   :ensure t
   :custom
   (org-roam-directory (file-truename (concat custom-user-doc-directory "/org-files/roam")))
+  (org-roam-completion-everywhere t)
   :bind (("C-c n l" . org-roam-buffer-toggle)
          ("C-c n f" . org-roam-node-find)
          ("C-c n g" . org-roam-graph)
          ("C-c n i" . org-roam-node-insert)
          ("C-c n c" . org-roam-capture)
          ;; Dailies
-         ("C-c n j" . org-roam-dailies-capture-today))
+         ("C-c n j" . org-roam-dailies-capture-today)
+         :map org-mode-map ;; following system crafters video, but C-M-i is already used by flyspell, maybe better to use another binding for flyspell, though
+         ("C-M-SPC" . completion-at-point)
+         )
   :config
   ;; If you're using a vertical completion framework, you might want a more informative completion interface
   (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
